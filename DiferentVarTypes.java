2cd33075b556b39ba58d48c0c6e522f4a4e73a05
Candoti01 <152418463+Candoti01@users.noreply.github.com>
2024-01-09 16:40:46 -0300

    Add files via upload

---
+++ DiferentVarTypes.java
@@ -0,0 +1,8 @@
+public class DiferentVarTypes {
+    public static void main(String[] args) {
+        int num = 2;
+        String nome = "tem";
+
+        System.out.println(nome + " " +num);
+    }
+}
---
+++ ReadOnlyInteger.java
@@ -0,0 +1,6 @@
+public class ReadOnlyInteger {
+    public static void main(String[] args) {
+       final Integer i = 123;
+       System.out.println("Valor de i é " + i);
+    }
+}
