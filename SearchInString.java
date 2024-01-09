e0becbda8a5c77f45d18ca3015fbecbe72dd1149
Candoti01 <152418463+Candoti01@users.noreply.github.com>
2024-01-09 16:43:16 -0300

    Add files via upload

---
+++ SearchInString.java
@@ -0,0 +1,6 @@
+public class SearchInString {
+    public static void main(String[] args) {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println(txt.indexOf("X"));
+    }
+}
