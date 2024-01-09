7d21b0b622760c9f3974bf7a413401adba26c20d
Candoti01 <152418463+Candoti01@users.noreply.github.com>
2024-01-09 16:25:04 -0300

    Add files via upload

---
+++ StringAndIntegerConcatenation.java
@@ -0,0 +1,8 @@
+public class StringAndIntegerConcatenation {
+    public static void main(String[] args) {
+       int num = 20;
+       String str = "Pessoas: ";
+
+       System.out.println(str + num);
+    }
+}
